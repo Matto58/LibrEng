1a582b80cb787cb5c5289b64de93afcf8c13f9f0
Matto <mattopootis@gmail.com>
2023-02-22 21:21:11 +0100

    Přidejte soubory projektu.

---
+++ libreng/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mattodev.LibrEng
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"LibrEng {Ver.eng} - https://github.com/Matto58/LibrEng");
+		}
+	}
+}
---
+++ libreng/Ver.cs
@@ -0,0 +1,7 @@
+﻿namespace Mattodev.LibrEng
+{
+	public static class Ver
+	{
+		public static string eng = "1.0";
+	}
+}
